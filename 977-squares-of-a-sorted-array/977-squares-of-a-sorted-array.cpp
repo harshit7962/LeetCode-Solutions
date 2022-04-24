9a75400303e8856f09ac0d0b2eeedcc2539d5c8b
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-04-24 10:38:04 +0530

    Time: 94 ms (5.09%), Space: 25.8 MB (95.13%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    static bool compareValue(int i, int j) {
+        return abs(i)<abs(j);
+    }
     vector<int> sortedSquares(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), compareValue);
         for(int i=0;i<nums.size();i++) nums[i]*=nums[i];
-        sort(nums.begin(), nums.end());
         return nums;
     }
 };
