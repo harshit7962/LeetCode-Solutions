2281863fd4c095c0dee1012f0995a3bda0f7a3fb
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-04-26 15:15:17 +0530

    Time: 7 ms (88.64%), Space: 10.1 MB (65.55%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,34 +1,9 @@
 class Solution {
 public:
-    int cost(vector<int> v1, vector<int> v2) {
-        int x1 = v1[0], y1 = v1[1], x2 = v2[0], y2 = v2[1];
-        int res=0;
-        while(x1!=x2 && y1!=y2) {
-            if(x1<x2) x1++;
-            else x1--;
-            if(y1<y2) y1++;
-            else y1--;
-            res++;
-        }
-        while(x1!=x2) {
-            if(x1<x2) x1++;
-            else x2++;
-            res++;
-        }
-        while(y1!=y2) {
-            if(y1<y2) y1++;
-            else y1--;
-            res++;
-        }
-        
-        return res;
-    }
-    
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int res=0;
-        for(int i=1;i<points.size();i++) {
-            res+=cost(points[i-1], points[i]);
-        }
+        for(int i=1;i<points.size();i++)
+            res+=max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
         
         return res;
     }
