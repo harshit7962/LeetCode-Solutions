484dd120f38c86476bb91892c3e110d524712200
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-03 20:32:34 +0530

    Time: 35 ms (41.27%), Space: 19.3 MB (14.39%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int curr=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]!=0) {
+                swap(nums[i], nums[curr]);
+                curr++;
+            }
+        }
+        
+    }
+};
