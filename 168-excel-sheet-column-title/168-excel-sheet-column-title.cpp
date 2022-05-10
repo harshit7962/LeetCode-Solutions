9082210fa77beb7a5916685003eba5f69ec65e2d
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-10 18:46:50 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (95.72%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -3,15 +3,15 @@ public:
     string convertToTitle(int columnNumber) {
         string res="";
         while(columnNumber>0) {
-            char c = '@' + (columnNumber%26);
-            if(columnNumber%26==0) {
+            char c = '@' + columnNumber%26;
+            if(c=='@') {
                 c = 'Z';
                 columnNumber--;
             }
+            
             res=c+res;
             columnNumber/=26;
         }
-        
         return res;
     }
 };
