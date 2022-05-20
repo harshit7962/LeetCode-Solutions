7760e19a0d1af2f521e0bad3e738871085bd3df5
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-20 20:01:07 +0530

    Time: 90 ms (55.36%), Space: 13 MB (98.28%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        if(n<4) return {};
-        
+        if(nums.size()<4) return {};
         sort(nums.begin(), nums.end());
         
         vector<vector<int>> res;
-        
+        int n=nums.size();
         for(int i=0;i<n-3;i++) {
             for(int j=i+1;j<n-2;j++) {
-                    int k=j+1, l=n-1;
-
-                    while(k<l) {
-                        int sum = (long long)target - ((long long )nums[i] + (long long)nums[j] + (long long)nums[k] + (long long)nums[l]);
-                        if(sum==0) {
-                            res.push_back({nums[i], nums[j], nums[k], nums[l]});
-                            while(k<l && nums[k+1] == nums[k]) k++;
-                            while(k<l && nums[l-1] == nums[l]) l--;
-                            k++;
-                            l--;
-                            
-                        } else if(sum<0) l--;
-                        else k++;
-                        
-                    }
-                while(j<n-2 && nums[j+1]==nums[j]) j++;
+                int k=j+1, l=n-1;
+                while(k<l) {
+                    int sum = (long long)target-((long long)nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[l]);
+                    if(sum==0) {
+                        res.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        while(k<l && nums[k]==nums[k+1]) k++;
+                        while(k<l && nums[l]==nums[l-1]) l--;
+                        k++;
+                        l--;
+                    } else if(sum<0) l--;
+                    else k++;
+                }
+                while(j<n-2 && nums[j]==nums[j+1]) j++;
             }
-            while(i<n-3 && nums[i+1]==nums[i]) i++;
+            while(i<n-3 && nums[i]==nums[i+1]) i++;
         }
-        
         return res;
-        
     }
 };
