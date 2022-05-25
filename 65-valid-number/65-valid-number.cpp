54a7bd5e76d097e05367d16691a1c43db7aad69a
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-25 14:40:44 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (76.84%) - LeetHub

--- 65-valid-number/65-valid-number.cpp
+++ 65-valid-number/65-valid-number.cpp
@@ -11,15 +11,25 @@ public:
             if(c>='0' && c<='9') digit=true;
             else if(c=='+' || c=='-') {
                 //Sign
+                //There can not be more than two occurances of Plus or Minus
                 if(sign==2) return false;
+                
+                //If not at the beginning, a Plus or Minus can only come after an e or E
                 if(i>0 && (s[i-1]!='e'&&s[i-1]!='E')) return false;
+                
+                //Plus or Minus cannot be the last occurance
                 if(i==s.size()-1) return false;
             } else if(c=='.') {
                 //Dot
+                
+                //Decimal cannot occur after an e or E or after any other dot
                 if(e||dot) return false;
+                
+                //Decimal Occuring at the end without occurance of the digit
                 if(i==s.size()-1 && !digit) return false;
                 dot=true;
             } else if(c=='e' || c=='E'){
+                //e occuring after another e or without occurance of a digit or at the end
                 if(e || !digit || i==s.size()-1) return false;
                 e=true;
             } else return false;
