74b7a4f9f040ca61f46cfadc6605333088f31ac7
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-25 11:25:52 +0530

    Time: 19 ms (7.08%), Space: 10.9 MB (13.01%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
     string simplifyPath(string path) {
+        path=path+"/";
         stack<string> st;
-        st.push("/");
-        
         string curr="";
-        for(int i=1;i<path.size();i++) {
-            if(path[i]!='/') curr+=path[i];
-            else if(path[i]=='/') {
-                if(curr==".") {
-                    st.pop();
-                }
-                else if(curr=="..") {
-                    st.pop();
-                    if(!st.empty()) st.pop();
-                } else {
-                    if(curr.size()!=0) st.push(curr);
-                }
-                if(st.empty() || st.top()!="/") st.push("/");
+        
+        for(int i=0;i<path.size();i++) {
+            if(path[i]=='/') {
+                if(curr=="..") {
+                    if(!st.empty())
+                        st.pop();
+                } else if(curr!="." && curr!="")
+                    st.push(curr);
                 curr="";
-            }
+            } else curr+=path[i];
         }
-        if(curr==".") {
-            st.pop();
-        }
-        else if(curr=="..") {
-            st.pop();
-            if(!st.empty()) st.pop();
-        } else {
-            if(curr.size()!=0) st.push(curr);
-        }
+        if(st.empty()) return "/";
         curr="";
-        
         while(!st.empty()) {
-            curr=st.top()+curr;
+            curr="/"+st.top()+curr;
             st.pop();
         }
-        
-        if(curr[0]!='/') curr='/'+curr;
-        if(curr[curr.size()-1]=='/') curr.pop_back();
-        if(curr.size()==0) return "/";
         
         return curr;
     }
