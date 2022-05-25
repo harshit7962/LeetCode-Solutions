6689aa3f439644d8040a6b14eb5713b35d3b2ae3
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-25 22:22:41 +0530

    Added solution - LeetHub

--- Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
+++ Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
@@ -9,10 +9,20 @@ class Solution
     public:
         vector<int> search(string pat, string txt)
         {
-            vector<int> res;
-            for(int i=0;i<txt.size()-pat.size()+1;i++) {
-                if(pat==txt.substr(i, pat.size())) res.push_back(i+1);
+            int hashpat=0, hashtxt=0;
+            for(int i=0;i<pat.size();i++) {
+                hashpat+=pat[i]-'A';
+                hashtxt+=txt[i]-'A';
             }
+            
+            vector<int> res;
+            if(hashpat==hashtxt && pat==txt.substr(0, pat.size())) res.push_back(1);
+            for(int i=pat.size();i<txt.size();i++) {
+                hashtxt+=(txt[i]-'A');
+                hashtxt-=(txt[i-pat.size()]-'A');
+                if(hashpat==hashtxt && pat==txt.substr(i-pat.size()+1, pat.size())) res.push_back(i-pat.size()+2);
+            }
+            
             if(res.size()==0) return {-1};
             return res;
         }
