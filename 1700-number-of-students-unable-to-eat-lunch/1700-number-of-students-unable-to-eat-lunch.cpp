33e464683290176ca4199cf197adea19d30e2c6d
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-02 13:18:14 +0530

    Time: 3 ms (72.56%), Space: 8.8 MB (28.41%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int count=0, curr1=0, curr2=0;
-        while(curr1<students.size() && curr2<sandwiches.size() && count<sandwiches.size()-curr2) {
-            if(students[curr1]==sandwiches[curr2]) {
-                curr1++;
-                curr2++;
+        queue<int> q;
+        for(int i=0;i<students.size();i++) q.push(students[i]);
+        
+        int curr=0, i=0, count=0;
+        while(i<sandwiches.size() && (count<=sandwiches.size()-curr)) {
+            if(q.front()==sandwiches[i]) {
+                q.pop();
+                curr++;
+                i++;
                 count=0;
-            } else {
-                students.push_back(students[curr1]);
-                curr1++;
-                count++;
             }
+            
+            count++;
+            q.push(q.front());
+            q.pop();
         }
         
-        return sandwiches.size()-curr2;
+        return students.size()-curr;
     }
 };
