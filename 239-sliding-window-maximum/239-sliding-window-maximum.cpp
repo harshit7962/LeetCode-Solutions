b928a60eab9ebfa7a5967ac8de5309bdb93deb97
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-06 21:13:21 +0530

    Time: 531 ms (25.66%), Space: 133.3 MB (56.42%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n= nums.size();
+        int n = nums.size();
         vector<int> nge(n, n);
-        
         stack<int> st;
         st.push(n-1);
         
         for(int i=n-2;i>=0;i--) {
             while(!st.empty() && nums[i]>=nums[st.top()]) st.pop();
-            
             if(!st.empty()) nge[i] = st.top();
             st.push(i);
         }
@@ -17,12 +15,11 @@ public:
         vector<int> res(n-k+1);
         int j=0;
         for(int i=0;i<=n-k;i++) {
-            if(j<i) j=i;
             
+            if(j<i) j=i;
             while(nge[j]<i+k) j = nge[j];
             
             res[i] = nums[j];
-            
         }
         
         return res;
