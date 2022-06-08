8f83c8a8f489533cdc4ef1bd5282c92defaf565a
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-08 22:49:15 +0530

    Time: 7 ms (83.71%), Space: 14.8 MB (81.75%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -14,33 +14,30 @@ public:
         if(list1==NULL) return list2;
         if(list2==NULL) return list1;
         
-        ListNode *head=NULL, *tail=NULL;
+        ListNode *head, *tail;
         
-        //Initializing the head node
-        if(list1->val < list2->val) {
+        if(list1->val<=list2->val) {
             head = list1;
             tail = list1;
             list1= list1->next;
         } else {
             head = list2;
             tail = list2;
-            list2= list2->next;
+            list2=list2->next;
         }
         
-        //Merging
         while(list1!=NULL && list2!=NULL) {
             if(list1->val <= list2->val) {
                 tail->next = list1;
-                tail = list1;
-                list1= list1->next;
+                tail = tail->next;
+                list1 = list1->next;
             } else {
                 tail->next = list2;
-                tail = list2;
-                list2= list2->next;
+                tail = tail->next;
+                list2 = list2->next;
             }
         }
         
-        //Appending remaining Linked List
         if(list1!=NULL) tail->next = list1;
         else tail->next = list2;
         
