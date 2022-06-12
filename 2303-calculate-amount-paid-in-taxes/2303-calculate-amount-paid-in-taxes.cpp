4c2b62289b634e5ec4d679316dddffc53a74bc63
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-12 10:17:24 +0530

    Time: 12 ms (75.00%), Space: 13.7 MB (50.00%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.cpp
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     double calculateTax(vector<vector<int>>& brackets, int income) {
-        if(income<=brackets[0][0]) return (double)income * (double)brackets[0][1]/100;
+        if(income==0) return 0.0;
         double total_tax=0;
-        for(int i=0;i<brackets.size();i++) {
-            if(i==0 || income>=(brackets[i][0]-brackets[i-1][0])) {
-                income-=(i==0)?brackets[i][0]:brackets[i][0]-brackets[i-1][0];
-                if(i==0) total_tax+= (double)brackets[0][0]*(double)brackets[0][1]/100;
-                else total_tax+=(double)(brackets[i][0]-brackets[i-1][0])*(double)brackets[i][1]/100;
+        int prev=0;
+        
+        for(vector v:brackets) {
+            int curr_val = v[0]-prev;
+            if(income > curr_val) {
+                income-=curr_val;
+                total_tax+=(double)curr_val*(double)v[1]/100;
+                prev = v[0];
             } else {
-                total_tax+=(double)income*(double)brackets[i][1]/100;
+                total_tax+=(double)income*(double)v[1]/100;
                 break;
             }
         }
