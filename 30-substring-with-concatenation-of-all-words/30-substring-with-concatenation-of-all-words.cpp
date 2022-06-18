5f66c2d29b57dded5ab7e66976d68debdeb12158
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-18 13:13:39 +0530

    Time: 319 ms (53.29%), Space: 149.3 MB (25.35%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,29 +1,53 @@
 class Solution {
 public:
-    bool checkContains(string s, unordered_map<string, int> mp, int wordSize) {
-        for(int i=0;i<=s.size()-wordSize;i=i+wordSize) {
-            string curr = s.substr(i, wordSize);
-            if(mp[curr]>0) mp[curr]--;
-            else return false;
+    bool contains(string s, unordered_map<string, int> mp, int i, int j, int ws) {
+        for(int x=i;x<=j;x=x+ws) {
+            mp[s.substr(x, ws)]--;
         }
+        
         for(auto x:mp) if(x.second!=0) return false;
+        
         return true;
     }
     
     vector<int> findSubstring(string s, vector<string>& words) {
+        //Total number of characters in our words list
+        int words_count = words.size()*words[0].size();
+        if(s.size()<words_count) return {};
+        
+        //We create a hashvalue to travel only if the hashvalue is satisfied...
+        int hashval=0;
         unordered_map<string, int> mp;
-        for(string str:words) mp[str]++;
+        for(string word:words) {
+            mp[word]++;
+            for(char c:word) hashval+=c-'a';
+        }
         
-        int windowSize = words[0].size()*words.size();
+        //For first index, we need to travel the string till number of characters...
+        int currval=0;
+        for(int i=0;i<words_count;i++) {
+            currval+=s[i]-'a';
+        }
         
-        if(s.size()<windowSize) return {};
         
         vector<int> res;
         
-        for(int i=0;i<=s.size()-windowSize;i++) {
-            string curr=s.substr(i, windowSize);
-            if(checkContains(curr, mp, words[0].size())) res.push_back(i);
+        if(hashval==currval) {
+            if(contains(s, mp, 0, words_count-1, words[0].size()))
+                res.push_back(0);
         }
+        
+        int i=0, j=words_count-1;
+        while(j<s.size()) {
+            currval-=(s[i++]-'a');
+            currval+=(s[++j]-'a');
+
+            if(currval==hashval) {
+                if(contains(s, mp, i, j, words[0].size()))
+                    res.push_back(i);
+            }
+        }
+        
         return res;
     }
 };
