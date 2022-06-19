8fc5bb801800e1625fa92732e5070043923ab10a
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-19 23:27:40 +0530

    Time: 4 ms (89.92%), Space: 6.8 MB (91.77%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> m1(256, 0), m2(256, 0);
-        for (int i=0; i < s.size(); ++i) {
-            if (m1[s[i]] != m2[t[i]]) return false;
-            m1[s[i]] = i+1;
-            m2[t[i]] = i+1;
+        vector<int> smap(128,0), tmap(128,0);
+        for(int i=0;i<s.size();i++) {
+            if(smap[s[i]]!=tmap[t[i]]) return false;
+            smap[s[i]]=i+1;
+            tmap[t[i]]=i+1;
         }
+        
         return true;
     }
 };
