e52b201cef06ed4142c788e0ecbc349096bd3f7b
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-25 08:22:20 +0530

    Time: 31 ms (82.54%), Space: 28.5 MB (5.88%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isIncreasing(vector<int> nums) {
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i]<nums[i-1]) return false; 
+        }
+        
+        return true;
+    }
+    
+    bool checkPossibility(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i]<nums[i-1]) {
+                int x=nums[i], y=nums[i-1];
+                nums[i]=y;
+                if(isIncreasing(nums)) return true;
+                else {
+                    nums[i]=x;
+                    nums[i-1]=x;
+                    return isIncreasing(nums);
+                }
+            }
+        }
+        
+        return true;
+    }
+};
