8a3ff91df929cf48555b4ec361a299661a314765
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-28 07:49:58 +0530

    Time: 89 ms (54.22%), Space: 17.2 MB (95.28%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -3,12 +3,20 @@ public:
     int minDeletions(string s) {
         vector<int> v(26, 0);
         for(char c:s) v[c-'a']++;
+        
         sort(v.begin(), v.end(), greater<int>());
         
         int res=0;
+        
         for(int i=1;i<26;i++) {
             if(v[i]==0) break;
-            while(v[i]!=0 && v[i]>=v[i-1]) {res++;v[i]--;}
+            if(v[i]>=v[i-1]) {
+                res+=v[i]-(v[i-1]-1);
+                v[i]=v[i-1]-1;
+            }
+            if(v[i]==0) {
+                while(v[i+1]!=0) {res+=v[i+1]; i++;}
+            }
         }
         
         return res;
