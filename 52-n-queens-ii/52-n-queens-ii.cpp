75f618e7c26d04249ecf0a48dc590774d7f9c55e
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-30 09:31:12 +0530

    Time: 4 ms (77.21%), Space: 6.2 MB (57.27%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-    int totalNQueens(int n) {
-        switch(n) {
-            case 1:
-                return 1;
-            case 2:
-                return 0;
-            case 3:
-                return 0;
-            case 4:
-                return 2;
-            case 5:
-                return 10;
-            case 6:
-                return 4;
-            case 7:
-                return 40;
-            case 8:
-                return 92;
+    int res=0;
+    
+    bool isSafe(vector<vector<char>> &board, int row, int col, int n) {
+        //Checking if current row is free of Queens
+        for(int i=0;i<col;i++) if(board[row][i]=='Q') return false;
+        
+        //Checking upper left diagonal
+        int i=row, j=col;
+        while(i>=0 && j>=0) {
+            if(board[i][j]=='Q') return false;
+            i--;
+            j--;
         }
-        return 352;
+        
+        //Checking lower left diagonal
+        i=row, j=col;
+        while(i<n && j>=0) {
+            if(board[i][j]=='Q') return false;
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+    void nQueens(vector<vector<char>> &board, int col, int n) {
+        if(col>=n) {res++; return;}
+        
+        for(int i=0;i<n;i++) {
+            if(isSafe(board, i, col, n)) {
+                board[i][col]='Q';
+                nQueens(board, col+1, n);
+                board[i][col]='.';
+            }
+        }
+        
+        return;
+    }
+    
+    int totalNQueens(int n) {
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+        
+        nQueens(board, 0, n);
+        
+        return res;
     }
 };
