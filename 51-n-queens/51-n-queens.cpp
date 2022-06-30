eccce7039ccdd88f64e1a35396e94e3b0c581820
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-30 09:15:24 +0530

    Time: 12 ms (48.61%), Space: 8 MB (32.60%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,47 +1,63 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    unordered_set<int> col;
-    unordered_set<int> negD;
-    unordered_set<int> posD;
-    int size;
+    vector<vector<string>> res;
     
-    void help(int n, vector<string>& prev)
-    {
-        if(n < 0)
-        {
-            ans.push_back(prev);
-            return;
+    bool isSafe(vector<vector<char>> &board, int row, int col) {
+        //checking if queen is already present in this row
+        for(int i=0;i<col;i++) if(board[row][i]=='Q') return false;
+        
+        //Checking upper left diagonal
+        int i=row, j=col;
+        while(i>=0 && j>=0) {
+            if(board[i][j]=='Q') return false;
+            i--;
+            j--;
         }
-        for(int i = 0; i < size; i++)
-        {
-            if(col.find(i) == col.end() && negD.find(n-i) == negD.end() && posD.find(n+i) == posD.end())
-            {
-                col.insert(i);
-                negD.insert(n-i);
-                posD.insert(n+i);
-                string s = "";
-                for(int j = 0; j < size; j++)
-                {
-                    if(i == j) s+='Q';
-                    else s+='.';
-                }
-                prev.push_back(s);
-                help(n-1,prev);
-                
-                // backtracking part
-                prev.pop_back();
-                col.erase(col.find(i));
-                negD.erase(negD.find(n-i));
-                posD.erase(posD.find(n+i));
+        
+        //Checking lower left diagonal
+        i=row, j=col;
+        while(i<board.size() && j>=0) {
+            if(board[i][j]=='Q') return false;
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+    void addToResult(vector<vector<char>> &board, int n) {
+        vector<string> ans;
+        string curr="";
+        for(int i=0;i<n;i++) {
+            curr = "";
+            for(int j=0;j<n;j++) {
+                curr+=board[i][j];
+            }
+            ans.push_back(curr);
+        }
+        
+        res.push_back(ans);
+    }
+    
+    void nQueens(vector<vector<char>> &board, int col, int n) {
+        if(col>=n) {addToResult(board, n); return;}
+        
+        for(int i=0;i<n;i++) {
+            if(isSafe(board, i, col)) {
+                board[i][col] = 'Q';
+                nQueens(board, col+1, n);
+                board[i][col] = '.';
             }
         }
+        
+        return;        
     }
     
     vector<vector<string>> solveNQueens(int n) {
-        size = n;
-        vector<string> prev;
-        help(n-1,prev);
-        return ans;
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+        
+        nQueens(board, 0, n);
+        
+        return  res;
     }
 };
