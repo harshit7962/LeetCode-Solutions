4c774050aa7696be1834aae728d4c9b1c4db1fd2
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-01 15:52:31 +0530

    Time: 14 ms (81.51%), Space: 10.5 MB (66.51%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+    
+    int findCeil(vector<int> &res, int k) {
+        int ans, low=0, high=res.size()-1;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(res[mid]>=k) {ans=mid;high=mid-1;}
+            else low=mid+1;
+        }
+        return ans;
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
+        vector<int> res;
+        res.push_back(nums[0]);
         
-        for(int i=1;i<n;i++) {
-            int res=0;
-            for(int j=0;j<i;j++) {
-                if(nums[j]<nums[i]) res = max(res, dp[j]);
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i]>res[res.size()-1]) res.push_back(nums[i]);
+            else {
+                int x = findCeil(res, nums[i]);
+                res[x] = nums[i];
             }
-            
-            dp[i] = res+1;
         }
         
-        int res=0;
-        for(int i:dp) res=max(res, i);
-        
-        return res;
+        return res.size();
     }
 };
