4eaa418d33fbba67d842d52d35580938ffec875b
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-02 09:43:18 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (51.48%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    int sumOfSquaredDigits(int n) {
+    int sumofSquares(int n) {
         int res=0;
         while(n>0) {
             res+=(n%10)*(n%10);
             n/=10;
         }
+        
         return res;
     }
     
     bool isHappy(int n) {
-        map<int, int> mp;
-        mp.insert({n,1});
-        int curr=sumOfSquaredDigits(n);
-        while(curr!=1) {
-            if(mp.find(curr)!=mp.end()) return false;
-            mp[curr]=1;
-            curr=sumOfSquaredDigits(curr);
+        unordered_set<int> st;
+        
+        while(true) {
+            int x = sumofSquares(n);
+            if(x==1) return true;
+            if(st.count(x)>0) return false;
+            st.insert(x);
+            n=x;
         }
-        return true;
+        
+        
+        return false;
     }
 };
