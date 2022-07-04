a2a7eb17769efe457ee246f554cf8e46b23d3c33
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-04 23:23:31 +0530

    Time: 3 ms (38.22%), Space: 6.1 MB (36.99%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int> dp(n+1, 0);
-        dp[0]=1;
-        dp[1]=1;
+        vector<int> dp(n+1, 1);
         
         for(int i=2;i<=n;i++) {
-            //Loop for filling our dp table
-            for(int j=0;j<i;j++){
-                dp[i]+=(dp[i-j-1]*dp[j]);
+            int res=0;
+            for(int j=0;j<i;j++) {
+                res+=dp[j]*dp[i-j-1];
             }
+            dp[i] = res;
         }
         
         return dp[n];
