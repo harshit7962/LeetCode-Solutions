8cbb29c04e56925c5eb2839723e18866ba8f9e2a
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-06 23:01:43 +0530

    Time: 1052 ms (15.76%), Space: 8.3 MB (6.58%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    bool dfs(vector<vector<char>>&board, vector<vector<bool>> &visited, string word, int ind, int row, int col) {
+        
+        if(ind>=word.size()) return true;
+        if(row<0 || col<0 || row>=board.size() || col>=board[0].size()) return false;
+        if(board[row][col]!=word[ind] || visited[row][col]) return false;
+        
+        visited[row][col] = true;
+
+        //Up check
+        if(dfs(board, visited, word, ind+1, row-1, col)) return true;
+        
+        //Down check
+        if(dfs(board, visited, word, ind+1, row+1, col)) return true;
+        
+        //Left check
+        if(dfs(board, visited, word, ind+1, row, col-1)) return true;
+        
+        //Right check
+        if(dfs(board, visited, word, ind+1, row, col+1)) return true;
+        
+        visited[row][col] =false;
+        
+        return false;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j] == word[0]) {
+                    vector<vector<bool>> visited(m, vector<bool>(n, false));
+                    
+                    if(dfs(board, visited, word, 0, i, j)) return true;
+                }
+            }
+        }
+        
+        
+        return false;
+    }
+};
