62b1d7a7bfb6854edab1646e6bb588815efb821a
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-07 12:47:11 +0530

    Time: 577 ms (5.01%), Space: 120.4 MB (42.01%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,31 +11,17 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(head->next==NULL) return true;
-        ListNode *middle=head, *tail=head;
-        while(tail->next!=NULL && tail->next->next!=NULL) {
-            tail = tail->next->next;
-            middle = middle->next;
-        }
+        string str="";
+        for(ListNode* curr=head;curr!=NULL;curr=curr->next) str+=(curr->val+48);
         
-        ListNode *curr = middle->next, *prev = middle;
+        int start=0, end=str.size()-1;
+        if(end==0 || end==-1) return true;
         
-        while(curr!=NULL) {
-            ListNode *next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+        while(start<end) {
+            if(str[start]!=str[end]) return false;
+            start++;
+            end--;
         }
-        middle->next->next = NULL;
-        middle->next = prev;
-        middle = middle->next;
-       
-        while(middle!=NULL) {
-            if(head->val != middle->val) return false;
-            middle = middle->next;
-            head = head->next;
-        }
-        
         return true;
     }
 };
