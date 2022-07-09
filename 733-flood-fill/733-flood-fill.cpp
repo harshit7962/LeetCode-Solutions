6d7ebcac1cd14340f9bba415438ee29d038f5fcd
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-09 20:32:18 +0530

    Time: 14 ms (59.20%), Space: 13.8 MB (98.80%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &image, int sr, int sc, int color, int prev) {
-        if(sr<0 || sc<0 || sr>=image.size() || sc>=image[0].size()) return;
+    void dfs(vector<vector<int>> &image, int row, int col, int color, int par_col) {
+        if(row<0 || col<0 || row>=image.size() || col>=image[0].size() || image[row][col]!=par_col) return;
         
-        image[sr][sc] = color;
+        image[row][col] = color;
         
-        if(sr!=0 && image[sr-1][sc]==prev) dfs(image, sr-1, sc, color, prev);
-        if(sc!=0 && image[sr][sc-1]==prev) dfs(image, sr, sc-1, color, prev);
-        if(sr<image.size()-1 && image[sr+1][sc]==prev) dfs(image, sr+1, sc, color, prev);
-        if(sc<image[0].size()-1 && image[sr][sc+1]==prev) dfs(image, sr,sc+1, color, prev);
+        //Moving Down
+        dfs(image, row+1, col, color, par_col);
+        
+        // Moving Up
+        dfs(image, row-1, col, color, par_col);
+        
+        //Moving left
+        dfs(image, row, col-1, color, par_col);
+        
+        //Moving right
+        dfs(image, row, col+1, color, par_col);
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc]!=color) {
-            dfs(image, sr, sc, color, image[sr][sc]);
-        }
+        if(image[sr][sc] == color) return image;
+        
+        dfs(image, sr, sc, color, image[sr][sc]);
         
         return image;
     }
