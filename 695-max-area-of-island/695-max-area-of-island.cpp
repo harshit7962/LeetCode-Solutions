03d716c4a26e856a5f0f38b1a339c275aba55cf4
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-09 21:07:27 +0530

    Time: 30 ms (53.54%), Space: 26.8 MB (13.01%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,19 +1,56 @@
 class Solution {
 public:
-    int bfs(vector<vector<int>> &grid, int row, int col) {
-        if(row<0||col<0||row>=grid.size()||col>=grid[0].size()||grid[row][col]==0) return 0;
-        grid[row][col]=0;
-        return 1+bfs(grid,row-1,col)+bfs(grid,row+1,col)+bfs(grid,row,col-1)+bfs(grid,row,col+1);
-    } 
-    
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size(), n=grid[0].size(), res=0;
+    int bfs(vector<vector<int>>&grid, int row, int col) {
+        int area = 0;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        grid[row][col] = 0;
         
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                res = max(res, bfs(grid, i, j));
+        while(!q.empty()) {
+            int curr_row = q.front().first, curr_col = q.front().second;
+            q.pop();
+            area++;
+            
+            //Moving UP
+            if(curr_row-1>=0 && grid[curr_row-1][curr_col]==1) {
+                q.push({curr_row-1, curr_col});
+                grid[curr_row-1][curr_col]=0;
+            }
+            
+            //Moving Down
+            if(curr_row+1<grid.size() && grid[curr_row+1][curr_col]==1) {
+                grid[curr_row+1][curr_col]=0;
+                q.push({curr_row+1, curr_col});
+            }
+            
+            //Moving Left
+            if(curr_col-1>=0 && grid[curr_row][curr_col-1]==1) {
+                q.push({curr_row, curr_col-1});
+                grid[curr_row][curr_col-1]=0;
+            }
+            
+            //Moving Right
+            if(curr_col+1<grid[0].size() && grid[curr_row][curr_col+1]==1) {
+                grid[curr_row][curr_col+1]=0;
+                q.push({curr_row, curr_col+1});
             }
         }
+        
+        return area;
+    }
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int res=0;
+        
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++) {
+                if(grid[i][j] == 1) {
+                    int x = bfs(grid, i, j);
+                    res = max(res, x);
+                }
+            }
+        }
+        
         
         return res;
     }
