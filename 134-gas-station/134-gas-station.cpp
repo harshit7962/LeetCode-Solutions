faa2a53008266852b4615f99337540a606a9fb09
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-10 10:55:44 +0530

    Time: 167 ms (15.10%), Space: 69.3 MB (89.26%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -3,16 +3,17 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int total_pet = 0, total_dis = 0;
         
-        for(int i=0;i<gas.size();i++) {
-            total_pet += gas[i];
-            total_dis += cost[i];
-        }
+        // for(int i=0;i<gas.size();i++) {
+        //     total_pet += gas[i];
+        //     total_dis += cost[i];
+        // }
         
-        if(total_pet<total_dis) return -1;
         
         int curr_pet=0, start=0, curr=0;
         
         while(curr<gas.size()) {
+            total_pet += gas[curr];
+            total_dis += cost[curr];
             curr_pet += gas[curr];
             
             int nxt = cost[curr];
@@ -27,6 +28,7 @@ public:
             curr++;
         }
         
+        if(total_pet<total_dis) return -1;
         return start;
     }
 };
