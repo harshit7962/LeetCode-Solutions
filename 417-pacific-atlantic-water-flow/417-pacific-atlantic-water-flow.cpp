8f0f95a32376d3714d333022a5415f4abe65b7c2
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-11 09:43:41 +0530

    Time: 61 ms (65.18%), Space: 17.4 MB (93.06%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<int>> &graph, vector<vector<bool>> &visited, int i, int j) {
-        if(i<0 || j<0 || i>=graph.size() || j>=graph[0].size() || visited[i][j]) return;
+    void dfs(vector<vector<int>>&grid, vector<vector<bool>>&visited, int row, int col) {
+        if(visited[row][col]) return;
+        visited[row][col] = true;
         
-        visited[i][j]=true;
-        
-        //Moving Up
-        if(i!=0 && graph[i][j]<=graph[i-1][j]) dfs(graph, visited, i-1, j);
-        
-        //Moving Down
-        if(i!=graph.size()-1 && graph[i][j]<=graph[i+1][j]) dfs(graph, visited, i+1, j);
-        
-        //Moving right
-        if(j!=graph[0].size()-1 && graph[i][j]<=graph[i][j+1]) dfs(graph, visited, i, j+1);
-        
-        //Moving left
-        if(j!=0 && graph[i][j]<=graph[i][j-1]) dfs(graph, visited, i, j-1);
-        
-        return;
+        if(row!=0 && grid[row][col] <= grid[row-1][col]) dfs(grid, visited, row-1, col);
+        if(col!=0 && grid[row][col] <= grid[row][col-1]) dfs(grid, visited, row, col-1);
+        if(row!=grid.size()-1 && grid[row][col] <= grid[row+1][col]) dfs(grid, visited, row+1, col);
+        if(col!=grid[0].size()-1 && grid[row][col] <= grid[row][col+1]) dfs(grid, visited, row, col+1);
     }
     
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m= heights.size(), n=heights[0].size();
-        vector<vector<bool>> pacific_vis(m, vector<bool>(n, false)), atlantic_vis(m, vector<bool>(n, false));
+        int m = heights.size(), n = heights[0].size();
+        vector<vector<bool>> pac_vis(m, vector<bool>(n, false)), atl_vis(m, vector<bool>(n, false));
         
-        for(int i=0;i<n;i++) {
-            dfs(heights, pacific_vis, 0, i);
-            dfs(heights, atlantic_vis, m-1, i);
+        for(int row=0;row<m;row++) {
+            if(!pac_vis[row][0]) {dfs(heights, pac_vis, row, 0);}
+            if(!atl_vis[row][n-1]) {dfs(heights, atl_vis, row, n-1);}
         }
         
-        for(int i=0;i<m;i++) {
-            dfs(heights, pacific_vis, i, 0);
-            dfs(heights, atlantic_vis, i, n-1);
+        for(int col=0;col<n;col++) {
+            if(!pac_vis[0][col]) {dfs(heights, pac_vis, 0, col);}
+            if(!atl_vis[m-1][col]) {dfs(heights, atl_vis, m-1, col);}
         }
         
         vector<vector<int>> res;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(pacific_vis[i][j] && atlantic_vis[i][j]) res.push_back({i, j});
+                if(pac_vis[i][j] && atl_vis[i][j]) {
+                    res.push_back({i, j});
+                }
             }
         }
         
