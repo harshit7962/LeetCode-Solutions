d22b8b0dfc24cc81d841e4d412a41b11db4e1318
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-12 23:00:00 +0530

    Time: 597 ms (27.50%), Space: 90.3 MB (70.33%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    
-    bool isSub(vector<vector<int>> &grid, int i, int j) {
-        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]==0) return true;
-        if(grid[i][j]==-1) return false;
+    bool dfs(vector<vector<int>>&grid, int i, int j) {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0) return true;
+        if(grid[i][j] == 1) return false;
         
-        grid[i][j]=0;
+        grid[i][j] = 0;
         
-        bool flag1 = isSub(grid, i+1, j);
-        bool flag2 = isSub(grid, i-1, j);
-        bool flag3 = isSub(grid, i, j+1);
-        bool flag4 = isSub(grid, i, j-1);
+        bool flag1 = dfs(grid, i-1, j);
+        bool flag2 = dfs(grid, i+1, j);
+        bool flag3 = dfs(grid, i, j-1);
+        bool flag4 = dfs(grid, i, j+1);
         
-        if(flag1&&flag2&&flag3&&flag4) return true;
-        grid[i][j]=-1;
-        return false;
+        return flag1 && flag2 && flag3 && flag4;        
     }
     
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int m=grid1.size(), n=grid1[0].size();
+        int m = grid1.size(), n = grid1[0].size();
         
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(grid1[i][j]==1 && grid2[i][j]==1) grid1[i][j]=2;
-                else if(grid2[i][j]==1) grid1[i][j]=-1;
-                else grid1[i][j]=0;
+                if(grid2[i][j]==1 && grid1[i][j]==1) grid2[i][j] = 2;
             }
         }
         
-        int res=0;
+        int res = 0;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(grid1[i][j]==2 && isSub(grid1, i, j)) {
-                    res++;
+                if(grid2[i][j]==2) {
+                    if(dfs(grid2, i, j)) res++;
                 }
             }
         }
