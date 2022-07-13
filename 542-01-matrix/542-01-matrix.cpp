2b42e904eaf39271ca16c311cd07d8255c88ae48
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-13 15:42:36 +0530

    Time: 154 ms (28.59%), Space: 31.6 MB (26.27%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
-    
-    void simulBFS(vector<vector<int>> mat, vector<vector<int>> &res, queue<pair<int, int>> &q) {
-        int count=-1;
-        vector<vector<bool>> visited(mat.size(), vector<bool>(mat[0].size(), false));
-        while(!q.empty()) {
-            int  c = q.size();
-            count++;
-            for(int x=0;x<c;x++) {
-                int i=q.front().first;
-                int j=q.front().second;
-                
-                q.pop();
-                
-                res[i][j]=count;
-                
-                if(i!=0 && mat[i-1][j]==1 && !visited[i-1][j]) {
-                    visited[i-1][j]=true;
-                    q.push({i-1, j});
-                }
-                if(j!=0 && mat[i][j-1]==1 && !visited[i][j-1]) {
-                    visited[i][j-1]=true;
-                    q.push({i, j-1});
-                }
-                if(i!=mat.size()-1 && mat[i+1][j]==1 && !visited[i+1][j]) {
-                    visited[i+1][j]=true;
-                    q.push({i+1, j});
-                }
-                if(j!=mat[0].size()-1 && mat[i][j+1]==1 && !visited[i][j+1]) {
-                    visited[i][j+1]=true;
-                    q.push({i, j+1});
-                }
-            }
-        }
-    }
-    
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size(), n=mat[0].size();
-        vector<vector<int>> res(m, vector<int>(n, 0));
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
         
-        queue<pair<int, int>> q;
+        queue<pair<pair<int, int>, int>> q;
         
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(mat[i][j]==0) q.push({i, j});
+                if(grid[i][j] == 0) q.push({{i, j}, 0});
+                else grid[i][j] = -1;
             }
         }
         
-        simulBFS(mat, res, q);
+        while(!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int dis = q.front().second;
+            
+            q.pop();
+            
+            if(row!=0 && grid[row-1][col]==-1) {
+                grid[row-1][col] = dis+1;
+                q.push({{row-1, col}, dis+1});
+            }
+            
+            if(col!=0 && grid[row][col-1]==-1) {
+                grid[row][col-1] = dis+1;
+                q.push({{row, col-1}, dis+1});
+            }
+            
+            if(row<m-1 && grid[row+1][col]==-1) {
+                grid[row+1][col] = dis+1;
+                q.push({{row+1, col}, dis+1});
+            }
+            
+            if(col<n-1 && grid[row][col+1]==-1) {
+                grid[row][col+1] = dis+1;
+                q.push({{row, col+1}, dis+1});
+            }
+        }
         
-        return res;
+        return grid;
     }
 };
