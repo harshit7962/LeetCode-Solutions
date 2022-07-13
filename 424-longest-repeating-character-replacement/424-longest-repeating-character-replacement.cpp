10045cac2891693afc343042b4980948c100bdfa
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-13 21:48:22 +0530

    Time: 11 ms (80.11%), Space: 7 MB (52.71%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left=0, right=0, res=0;
-        vector<int> char_count(26, 0);
+        int left=0, right=0, res=0, curr_max=0;
         
-        int max_count=0;
+        vector<int> char_count(26, 0);
         
         while(right<s.size()) {
             char_count[s[right]-'A']++;
-            max_count = max(max_count, char_count[s[right]-'A']);
+            curr_max = max(curr_max, char_count[s[right]-'A']);
             
-            right++;
-            int window_size = right-left;
-            
-            if(window_size-max_count<=k) {
-                res = max(res, right-left);
-            } else {
+            int window_size = right-left+1;
+            if(window_size-curr_max > k) {
                 char_count[s[left]-'A']--;
                 left++;
             }
+            
+            res = max(res, right-left+1);
+            right++;
         }
         
         return res;
