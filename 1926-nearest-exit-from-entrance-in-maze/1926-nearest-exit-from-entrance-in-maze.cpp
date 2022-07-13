87444f0c86ab44daa21358d664b3ef4368c74d46
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-13 16:55:21 +0530

    Time: 270 ms (23.07%), Space: 30.5 MB (54.37%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,49 +1,44 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int m=maze.size(), n=maze[0].size();
+        int m = maze.size(), n = maze[0].size();
+        
         queue<pair<pair<int, int>, int>> q;
         
-        q.push({{entrance[0], entrance[1]}, 0});
-        
+        q.push({{entrance[0], entrance[1]}, 1});
         maze[entrance[0]][entrance[1]] = '+';
         
         while(!q.empty()) {
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int c = q.front().second;
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int dis = q.front().second;
             
             q.pop();
-             
-            //Moving Up
-            if(i!=0 && maze[i-1][j]=='.') {
-                if(i-1==m-1 || i-1==0 || j==0|| j==n-1) return c+1;
-                maze[i-1][j] = '+';
-                q.push({{i-1, j},c+1});
+            
+            if(row!=0 && maze[row-1][col]=='.') {
+                if(row-1==0 || col==0 || row-1 == m-1 || col == n-1)  return dis;
+                maze[row-1][col] = '+';
+                q.push({{row-1, col},dis+1});
             }
             
-            //Moving Down
-            if(i!=m-1 && maze[i+1][j]=='.') {
-                if(i+1==m-1 || i+1==0 || j==0|| j==n-1) return c+1;
-                maze[i+1][j] = '+';
-                q.push({{i+1,j},c+1});
+            if(col!=0 && maze[row][col-1]=='.') {
+                if(row==0 || col-1==0 || row == m-1 || col-1 == n-1)  return dis;
+                maze[row][col-1] = '+';
+                q.push({{row, col-1},dis+1});
             }
             
-            //Moving Left
-            if(j!=0 && maze[i][j-1]=='.') {
-                if(i==m-1 || i==0 || j-1==0|| j-1==n-1) return c+1;
-                maze[i][j-1] ='+';
-                q.push({{i, j-1}, c+1});
+            if(row!=m-1 && maze[row+1][col]=='.') {
+                if(row+1==0 || col==0 || row+1 == m-1 || col == n-1)  return dis;
+                maze[row+1][col] = '+';
+                q.push({{row+1, col},dis+1});
             }
             
-            //Moving Right
-            if(j!=n-1 && maze[i][j+1]=='.') {
-                if(i==m-1 || i==0 || j+1==0|| j+1==n-1) return c+1;
-                maze[i][j+1]='+';
-                q.push({{i,j+1}, c+1});
+            if(col!=n-1 && maze[row][col+1]=='.') {
+                if(row==0 || col+1==0 || row == m-1 || col+1 == n-1)  return dis;
+                maze[row][col+1] = '+';
+                q.push({{row, col+1},dis+1});
             }
         }
-        
         
         return -1;
     }
