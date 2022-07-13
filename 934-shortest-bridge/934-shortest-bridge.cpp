b71b65d16db21300d03a57502f993bf1425f17f7
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-13 16:03:25 +0530

    Time: 74 ms (65.64%), Space: 22.6 MB (35.57%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -1,74 +1,68 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &grid, int i, int j, queue<pair<pair<int, int>, int>> &q) {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid.size() || grid[i][j]!=1) return;
+    void dfs(vector<vector<int>> &grid, queue<pair<pair<int, int>, int>>&q, int row, int col, int n) {
+        if(row<0 || col<0 || row==n || col==n || grid[row][col]!=1) return;
         
-        grid[i][j] = 2;
-        q.push({{i, j}, 0});
+        q.push({{row, col}, 0});
+        grid[row][col] = -1;
         
-        dfs(grid, i-1, j, q);
-        dfs(grid, i, j-1, q);
-        dfs(grid, i+1, j, q);
-        dfs(grid, i, j+1, q);
-    }
-    
-    int simulBFS(vector<vector<int>> grid, queue<pair<pair<int, int>, int>> &q) {
-        while(!q.empty()) {
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int curr_len = q.front().second;
-            
-            q.pop();            
-            
-            //Moving up
-            if(i!=0 && grid[i-1][j]!=2) {
-                if(grid[i-1][j]==1) return curr_len+1;
-                q.push({{i-1, j}, curr_len+1});
-                grid[i-1][j]=2;
-            }
-            
-            //Moving Down
-            if(i!=grid.size()-1 && grid[i+1][j]!=2) {
-                if(grid[i+1][j]==1) return curr_len+1;
-                q.push({{i+1, j}, curr_len+1});
-                grid[i+1][j]=2;
-            }
-            
-            //Moving Left
-            if(j!=0 && grid[i][j-1]!=2) {
-                if(grid[i][j-1]==1) return curr_len+1;
-                q.push({{i, j-1}, curr_len+1});
-                grid[i][j-1]=2;
-            }
-            
-            //Moving Right
-            if(j!=grid.size()-1 && grid[i][j+1]!=2) {
-                if(grid[i][j+1]==1) return curr_len+1;
-                q.push({{i, j+1}, curr_len+1});
-                grid[i][j+1]=2;
-            }
-        }
-        
-        return -1;
+        dfs(grid, q, row-1, col, n);
+        dfs(grid, q, row, col-1, n);
+        dfs(grid, q, row+1, col, n);
+        dfs(grid, q, row, col+1, n);        
     }
     
     int shortestBridge(vector<vector<int>>& grid) {
-        int n=grid.size();
+        int n = grid.size();
+        
         queue<pair<pair<int, int>, int>> q;
-        //We will mark any one cluster of islands as 2 in our grid, and this can be done either using dfs traversal or bfs traversal
-        bool flag=false;
+        
+        bool flag = false;
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
-                if(grid[i][j]==1) {
-                    dfs(grid, i, j, q);
-                    flag=true;
+                if(grid[i][j] == 1) {
+                    dfs(grid, q, i, j, n);
+                    flag = true;
                     break;
                 }
             }
             if(flag) break;
         }
         
-        //For each cell marked as 2, we find the nearest 1 to it, and store it in our result...
-        return simulBFS(grid, q)-1;
+        
+        //Initiating Simultaneous BFS
+        while(!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int dis = q.front().second;
+            
+            q.pop();
+            
+            if(row!=0 && grid[row-1][col]!=-1) {
+                if(grid[row-1][col]==1) return dis;
+                grid[row-1][col] = -1;
+                q.push({{row-1, col}, dis+1});
+            }
+            
+            if(col!=0 && grid[row][col-1]!=-1) {
+                if(grid[row][col-1]==1) return dis;
+                grid[row][col-1] = -1;
+                q.push({{row, col-1}, dis+1});
+            }
+            
+            if(row!=n-1 && grid[row+1][col]!=-1) {
+                if(grid[row+1][col]==1) return dis;
+                grid[row+1][col] = -1;
+                q.push({{row+1, col}, dis+1});
+            }
+            
+            if(col!=n-1 && grid[row][col+1]!=-1) {
+                if(grid[row][col+1]==1) return dis;
+                grid[row][col+1] = -1;
+                q.push({{row, col+1}, dis+1});
+            }
+        }
+        
+        return -1;
     }
 };
