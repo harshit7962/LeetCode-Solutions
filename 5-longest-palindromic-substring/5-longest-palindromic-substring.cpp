81c5d420de0e3eff02ca1daf0a67718683f99575
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-14 09:51:09 +0530

    Time: 25 ms (87.57%), Space: 14.3 MB (49.46%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string res="";
+        string res = "";
+        
         int left, right;
-        for(int i=0;i<s.size();i++) {
-            //Checking for odd sized string
-            left=i, right=i;
-            while(left>=0 && right<s.size() && s[left]==s[right]) {
-                if(res.size()<right-left+1) res = s.substr(left, right-left+1);
+        for(int i=0;i<s.size();i++) {            
+            //Odd sized palindromes
+            left =i-1, right=i+1;
+            while(left>=0 && right<s.size() && s[left] == s[right]) {
                 left--;
                 right++;
             }
+            if(res.size()<right-left-1) res = s.substr(left+1, right-left-1);
             
-            //Checking for even sized string
-            left=i, right=i+1;
-            while(left>=0 && right<s.size() && s[left]==s[right]) {
-                if(res.size()<right-left+1) res = s.substr(left, right-left+1);
+            //Even sized palindromes
+            left = i, right = i+1;
+            while(left>=0 && right<s.size() && s[left] == s[right]) {
                 left--;
                 right++;
             }
+            if(res.size()<right-left-1) res = s.substr(left+1, right-left-1);
         }
         
         return res;
