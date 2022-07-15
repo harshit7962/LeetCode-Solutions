d4a86650df0b54d889fa2ef496dc83344133fd00
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-15 22:20:58 +0530

    Time: 24 ms (80.34%), Space: 14.9 MB (64.98%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,63 +1,63 @@
 class Solution {
 public:
-    
-    vector<int> bfs(vector<int> redAdj[], vector<int> blueAdj[], int n) {
-        vector<int> distance(n, -1);
-        distance[0] = 0;
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        vector<int> res(n, -1);
+        res[0] = 0;
         
         queue<pair<int, char>> q;
         vector<bool> redVisited(n, false), blueVisited(n, false);
         
-        int dist=1;
-        for(int y:redAdj[0]) {
-            q.push({y, 'R'});
-            redVisited[y] = true;
+        vector<int> redAdj[n], blueAdj[n];
+        
+        for(auto v: redEdges) {
+            redAdj[v[0]].push_back(v[1]);
+        }
+        for(auto v:blueEdges) {
+            blueAdj[v[0]].push_back(v[1]);
         }
         
-        for(int y:blueAdj[0]) {
-            q.push({y, 'B'});
-            blueVisited[y] = true;
+        for(int i:redAdj[0]){
+            q.push({i, 'R'});
+            redVisited[i] = true;
         }
         
+        for(int i:blueAdj[0]) {
+            q.push({i, 'B'});
+            blueVisited[i] = true;
+        }
+        
+        int dist = 1;
         while(!q.empty()) {
             int c = q.size();
             for(int i=0;i<c;i++) {
-                
-                int currNode = q.front().first;
-                char color = q.front().second;
+                int node = q.front().first;
+                char col = q.front().second;
                 
                 q.pop();
                 
-                if(distance[currNode]==-1) distance[currNode] = dist;
-
-                if(color=='R') {
-                    for(int y:blueAdj[currNode]) {
-                        if(!blueVisited[y]) {
-                            q.push({y, 'B'});
-                            blueVisited[y] = true;
-                        }
-                    }
-                } else {
-                    for(int y:redAdj[currNode]) {
+                if(res[node]==-1) res[node] = dist;
+                
+                if(col == 'B') {
+                    for(int y:redAdj[node]) {
                         if(!redVisited[y]) {
                             q.push({y, 'R'});
                             redVisited[y] = true;
                         }
                     }
+                } else {
+                    for(int y:blueAdj[node]) {
+                        if(!blueVisited[y]) {
+                            q.push({y, 'B'});
+                            blueVisited[y] = true;
+                        }
+                    }
                 }
-
+                
             }
-            dist++;            
+            
+            dist++;
         }
-        return distance;
-    }
-    
-    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
-        vector<int> redAdj[n], blueAdj[n];
         
-        for(int i=0;i<redEdges.size();i++) redAdj[redEdges[i][0]].push_back(redEdges[i][1]);
-        for(int i=0;i<blueEdges.size();i++) blueAdj[blueEdges[i][0]].push_back(blueEdges[i][1]);
-        
-        return bfs(redAdj, blueAdj, n);
+        return res;
     }
 };
