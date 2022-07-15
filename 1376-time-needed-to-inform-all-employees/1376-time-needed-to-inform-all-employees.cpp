db11ed932e0b0f6f1e6bdf00fb2399ac74702f9f
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-15 11:09:15 +0530

    Time: 693 ms (25.08%), Space: 117.4 MB (75.67%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    
-    int final_res = INT_MAX;
-    
-    int bfs(vector<int> adj[], vector<int> informTime, int src) {
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<int> adj[n];
+        
+        for(int i=0;i<n;i++) {
+            if(manager[i]!=-1)
+            adj[manager[i]].push_back(i);
+        }
+        
+        
         queue<pair<int, int>> q;
-        q.push({src, informTime[src]});
+        q.push({headID, informTime[headID]});
         
         int res=0;
         
         while(!q.empty()) {
-            int node = q.front().first;
+            int  node = q.front().first;
             int time = q.front().second;
             
             q.pop();
+            
             res = max(res, time);
             
-            for(int subordinates: adj[node]) {
-                q.push({subordinates, time+informTime[subordinates]});
+            for(int y:adj[node]) {
+                q.push({y, time+informTime[y]});
             }
         }
         
         return res;
-    }
-    
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        if(n==1) return  0;
-        vector<int> adj[n];
-        
-        for(int i=0;i<manager.size();i++) {
-            if(manager[i]!=-1) {
-                adj[manager[i]].push_back(i);
-            }
-        }
-        
-        return bfs(adj, informTime, headID);
     }
 };
