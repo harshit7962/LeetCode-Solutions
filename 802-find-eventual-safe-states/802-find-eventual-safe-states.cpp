9806a13fe2cb084b37edfdc794c1730e57500aa8
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-15 21:54:10 +0530

    Time: 356 ms (19.92%), Space: 46.8 MB (81.78%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    bool hasCycle(vector<vector<int>> &graph,vector<bool> &visited, vector<bool> &recst, vector<bool> &part, int src) {
+    
+    bool dfs(vector<vector<int>>&graph, int src, vector<bool>&visited, vector<bool>&recst, vector<bool>&part) {
         visited[src] = true;
         recst[src] = true;
         
-        for(auto y:graph[src]) {
-            if(!visited[y]) {
-                if(hasCycle(graph, visited, recst, part, y)) {return part[src]=true;}
-            } else if(recst[y]) {return part[src]=true;}
+        for(int i:graph[src]) {
+            if(!visited[i]) {
+                if(dfs(graph, i, visited, recst, part)) {return part[src] = true;}
+            }
+            else if(recst[i]) {return part[src] = true;}
         }
         
-        recst[src]= false;
+        recst[src] = false;
         return false;
     }
     
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        
         vector<bool> visited(n, false), recst(n, false), part(n, false);
-        vector<int> res;
         
         for(int i=0;i<n;i++) {
-            if(!visited[i]){
-                hasCycle(graph, visited, recst, part, i);
+            if(!visited[i]) {
+                dfs(graph, i, visited, recst, part);
             }
         }
         
-        for(int i=0;i<n;i++) if(!part[i]) res.push_back(i);
-
+        vector<int> res;
+        for(int i=0;i<n;i++) {
+            if(!part[i]) res.push_back(i);
+        }
+        
         return res;
     }
 };
