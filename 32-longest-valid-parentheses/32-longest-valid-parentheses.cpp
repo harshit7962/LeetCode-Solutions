ef588a53fa2f7161ec4b23edb326391f3ab3d63b
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-22 20:15:28 +0530

    Time: 4 ms (75.18%), Space: 7.3 MB (38.57%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        if(s.size()==0) return 0;
-        vector<int> dp(s.size(), 0);
-        int ans = 0;
+        //We will find the longest valid parentheses ending with index i for each index...
+        int n = s.size(), ans = 0;
+        vector<int> dp(n, 0);
         
-        for(int i=1;i<s.size();i++) {
-            if(s[i]==')') {
+        for(int i=1;i<n;i++) {
+            //We know a valid parentheses will end only with closing bracket...
+            if(s[i] == ')') {
+                //If the prev character is opening bracket we can get the current longest by adding 2 to previous' previous value...
                 if(s[i-1]=='(') {
-                    dp[i] = (i-2>=0)?dp[i-2]+2:2;
-                } else if(i-1-dp[i-1]>=0 && s[i-dp[i-1]-1]=='(') {
-                    dp[i] = dp[i-1] + (i - dp[i-1]>=2?dp[i-dp[i-1]-2]:0)+2;
+                    dp[i] = 2 + (i==1?0:dp[i-2]);
+                } else if(i-dp[i-1]-1>=0 && s[i-dp[i-1]-1]=='(') {
+                    //If prev character is not opening bracket, then we can check it with the previous character's last valid bracket, using dp array's indexing...
+                    dp[i] = 2 + (i-dp[i-1]-1>0?dp[i-dp[i-1]-2]:0) + dp[i-1];
                 }
             }
+            
             ans = max(ans, dp[i]);
         }
         
