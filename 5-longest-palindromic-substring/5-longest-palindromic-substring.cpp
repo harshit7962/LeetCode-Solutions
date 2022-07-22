31f3f6907a9dfefee1965cdaee9c409a007d0b45
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-07-22 20:03:13 +0530

    Time: 40 ms (74.22%), Space: 14.3 MB (49.46%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -3,25 +3,23 @@ public:
     string longestPalindrome(string s) {
         string res = "";
         
-        int left, right;
-        for(int i=0;i<s.size();i++) {            
-            //Odd sized palindromes
-            left =i-1, right=i+1;
+        for(int i=0;i<s.size();i++) {
+            //Odd Sized palindromic substrings
+            int left = i-1, right = i+1;
             while(left>=0 && right<s.size() && s[left] == s[right]) {
                 left--;
                 right++;
             }
-            if(res.size()<right-left-1) res = s.substr(left+1, right-left-1);
+            if(res.size() < right-left-1) res = s.substr(left+1, right-left-1);
             
-            //Even sized palindromes
             left = i, right = i+1;
             while(left>=0 && right<s.size() && s[left] == s[right]) {
                 left--;
                 right++;
             }
-            if(res.size()<right-left-1) res = s.substr(left+1, right-left-1);
+            if(res.size() < right-left-1) res = s.substr(left+1, right-left-1);
         }
         
-        return res;
+        return res;        
     }
 };
