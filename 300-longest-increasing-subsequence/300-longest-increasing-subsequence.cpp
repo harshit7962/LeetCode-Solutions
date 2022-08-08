4ca0043db3b0f1621a80e069e597cd4a1ae44df2
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-08-08 11:36:23 +0530

    Time: 8 ms (95.75%), Space: 10.5 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    
-    int findCeil(vector<int> &res, int k) {
-        int ans, low=0, high=res.size()-1;
-        while(low<=high) {
+    int findCiel(vector<int> &res, int n) {
+        int ans = 0, low = 0, high = res.size()-1;
+        while(low <= high) {
             int mid = low + (high-low)/2;
-            if(res[mid]>=k) {ans=mid;high=mid-1;}
-            else low=mid+1;
+            
+            if(res[mid]>=n) {
+                ans = mid;
+                high = mid-1;
+            } else low = mid+1;
         }
+        
         return ans;
     }
     
     int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
         vector<int> res;
         res.push_back(nums[0]);
         
-        for(int i=1;i<nums.size();i++) {
-            if(nums[i]>res[res.size()-1]) res.push_back(nums[i]);
+        for(int i=1;i<n;i++) {
+            if(nums[i] > res[res.size()-1]) res.push_back(nums[i]);
             else {
-                int x = findCeil(res, nums[i]);
-                res[x] = nums[i];
+                res[findCiel(res, nums[i])] = nums[i];
             }
         }
         
